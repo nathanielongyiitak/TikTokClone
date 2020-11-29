75fa8eb1e25edbc909e3fa8fdcc6d58d699373dd
nathanielongyiitak <hinathanielong@gmail.com>
2020-11-29 16:28:25 +0800

    Post component done

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import Post from '../../components/Post';
+
+const post1 = {
+  id: 'p1',
+  user: {
+    id: 'v1',
+    username: 'daviddobrik',
+  },
+  description: 'hahahah oh boy @borat',
+  songName: 'NF - The search',
+  songImage: 'https://picsum.photos/id/237/536/354',
+  likes: 123,
+  comments: 12,
+  shares: 44,
+  videoUri:
+    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+};
+
+const post2 = {
+  id: 'p1',
+  user: {
+    id: 'v1',
+    username: 'vadimsavin',
+  },
+  description: 'hello there',
+  songName: 'NF - The search',
+  songImage: 'https://picsum.photos/id/237/536/354',
+  likes: 123,
+  comments: 12,
+  shares: 44,
+  videoUri:
+    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+};
 
 const Home = () => {
   return (
     <View>
-      <Text>Home screen</Text>
+      <Post post={post2} />
     </View>
   );
 };
